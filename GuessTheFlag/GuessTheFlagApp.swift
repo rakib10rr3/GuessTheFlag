1645cdb5dab614fb24a5dcfb325f3a7a11fb02cd
Rakibul Huda <rakib10rr3@gmail.com>
2020-12-28 18:42:50 +0600

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Rakibul Huda on 28/12/20.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
